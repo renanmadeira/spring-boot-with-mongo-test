6e5b8df6dab8bf1d11f987f70272e1cfa543a7e1
Renan Madeira <renanmadeira@gmail.com>
2018-08-20 23:23:27 -0300

    primeiro comit, projeto criado

---
+++ src/main/java/com/renanmadeira/conmongodb/ComMongoDbApplication.java
@@ -0,0 +1,12 @@
+package com.renanmadeira.conmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComMongoDbApplication.class, args);
+	}
+}
